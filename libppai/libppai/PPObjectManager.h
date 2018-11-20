20603ded7ce5d18863252492493ace226f751342
*p@KGCA007 <pointerphile@gmail.com>
2018-11-20 14:31:49 +0900

    refactoring

--- libppai/libppai/PPObjectManager.h
+++ libppai/libppai/PPObjectManager.h
@@ -7,20 +7,15 @@ namespace PP {
 	class PPObjectManager : public PP::PPSingleton<PPObjectManager>
 	{
 	public:
-		std::list<PP::PPObject*> m_listpObject;
 		std::map<PP::PPHandle, PP::PPObject*> m_mapObject;
 		uint64_t m_iID = 0;
 	public:
 		PPObjectManager();
 		~PPObjectManager();
 	public:
-		//void* push_back_Pet(int iPosition);
 		PPHandle push_back_Pet_m(int iPosition);
-		//void* push_back_Food(int iPosition, int iQuantity);
 		PPHandle push_back_Food_m(int iPosition, int iQuantity);
-		//bool CheckElement(PP::PPObject* pObject);
 		bool CheckElement_m(PP::PPHandle Handle);
-		//void erase(PP::PPObject* pObject);
 		void erase_m(PP::PPHandle Handle);
 		void* GetpObject(PP::PPHandle Handle);
 	};
